ba3654d19ce55ef26c16730d586171ba4c1b7128
jhonjonh <john.gerber@live.com>
2021-10-14 16:02:17 -0300

    ajustes finais

--- src/app/department/department.component.ts
+++ src/app/department/department.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { Department } from '../department';
@@ -7,34 +8,80 @@ import { DepartmentService } from '../department.service';
 @Component({
   selector: 'app-department',
   templateUrl: './department.component.html',
-  styleUrls: ['./department.component.css']
+  styleUrls: ['./department.component.css'],
 })
 export class DepartmentComponent implements OnInit {
-
   depName: string = '';
   departments: Department[] = [];
   depEdit: Department = null;
 
   private unsubscribe$: Subject<any> = new Subject();
 
-  constructor(private departmentService: DepartmentService) { }
+  constructor(
+    private departmentService: DepartmentService,
+    private snackbar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
-    this.departmentService.get()
-    .pipe(takeUntil(this.unsubscribe$))
-    .subscribe((deps) => this.departments = deps)
+    this.departmentService
+      .get()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((deps) => (this.departments = deps));
   }
 
-  clearFields(){
+  save() {
+    if (this.depEdit) {
+      this.departmentService
+        .update({ name: this.depName, _id: this.depEdit._id })
+        .subscribe(
+          (dep) => {
+            this.notify('UPADATED!');
+          },
+          (err) => {
+            this.notify('ERROR');
+            console.log(err);
+          }
+        );
+    } else {
+      this.departmentService.add({ name: this.depName }).subscribe(
+        (dep) => {
+          console.log(dep);
+          this.notify('INSERTED!');
+        },
+        (err) => {
+          console.error(err);
+        }
+      );
+    }
+    this.clearFields();
+  }
+
+  edit(dep: Department) {
+    this.depName = dep.name;
+    this.depEdit = dep;
+  }
+
+  delete(dep: Department) {
+    this.departmentService.del(dep).subscribe(
+      () => this.notify('REMOVED!'),
+      (err) => this.notify(err.error.msg)
+    );
+  }
+
+  clearFields() {
     this.depName = '';
     this.depEdit = null;
   }
 
-  cancel(){
+  cancel() {
     this.clearFields();
   }
 
-  
+  notify(msg: string) {
+    this.snackbar.open(msg, 'OK', { duration: 3000 });
+  }
 
-
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+  }
 }
